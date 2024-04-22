a1faa1fa26b6efd7459e3dfbf0e13f00f5d26cbf
neha2801-create <77967216+neha2801-create@users.noreply.github.com>
2024-04-22 13:36:35 -0400

    settings change

--- Backend/NoteCanvas/NoteCanvas/settings.py
+++ Backend/NoteCanvas/NoteCanvas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b!04ao$$d8t-f&!eh@zy9*(b!wkcvi-jw2+@!ju*&dz(zrq$j3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -131,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
